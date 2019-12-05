b215364a18d42b1acc01132d0a22cdb6dc3a3026
yutify <yu777developer@gmail.com>
2019-12-05 16:08:29 +0900

    updatet font

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <div class="loader">Loading...</div>
     <header class="header-block">
       <div class="header-boxes">
         <div class="header-box1"></div>
@@ -15,50 +16,54 @@
     </header>
     <div class>
       <router-link class="text1" to="/about">
-        <img src="../../public/gopher-st-image.png" width="50px" height="50px"/>
-        WhoAmI
+        <!-- <img src="../../public/gopher-st-image.png" width="50px" height="50px"/> -->
+        Who Am I
         </router-link>
       <router-link class="text2" to="worked_at">
-        <img src="../../public/company-image.png" width="50px" height="50px"/>
-        WorkedAt
+        <!-- <img src="../../public/company-image.png" width="50px" height="50px"/> -->
+        Work
         </router-link>
     </div>
     <div>
       <router-link class="text1" to="/related_service">
-        <img src="../../public/app-image.png" width="50px" height="50px"/>
-        RelatedService
+        <!-- <img src="../../public/app-image.png" width="50px" height="50px"/> -->
+        Service
         </router-link>
         <a class="text2" href="https://github.com/yukato7">
-        <img src="../../public/github-image.png" width="50px" height="50px"/>
+        <!-- <img src="../../public/github-image.png" width="50px" height="50px"/> -->
         Github
         </a>
     </div>
     <div>
       <a class="text3" href="https://dart-journey.tk/">
-      <img src="../../public/dart-image.png" width="50px" height="50px"/>
-      DartJourney
+      <!-- <img src="../../public/dart-image.png" width="50px" height="50px"/> -->
+      Dart Journey
       </a>
       <a class="text4" href="https://new-gengo.com">
-      <img src="../../public/gengo-image.svg" width="50px" height="50px"/>
+      <!-- <img src="../../public/gengo-image.svg" width="50px" height="50px"/> -->
       Gengo
       </a>
     </div>
     <div>
       <a class="text3" href="https://twitter.com/yukato7777">
-      <img src="../../public/twitter-image.png" width="50px" height="50px"/>
+      <!-- <img src="../../public/twitter-image.png" width="50px" height="50px"/> -->
       Twitter
       </a>
       <a class="text4" href="https://medium.com/@yutify">
-      <img src="../../public/medium-image.png" width="50px" height="50px"/>
+      <!-- <img src="../../public/medium-image.png" width="50px" height="50px"/> -->
       Medium
       </a>
       <a class="text5" href="https://note.com/yu1013sp">
-      <img src="../../public/note-image.png" width="50px" height="50px"/>
+      <!-- <img src="../../public/note-image.png" width="50px" height="50px"/> -->
       Note
       </a>
       <a class="text6" href="https://qiita.com/yu19991013">
-      <img src="../../public/qiita-image.jpeg" width="50px" height="50px"/>
+      <!-- <img src="../../public/qiita-image.jpeg" width="50px" height="50px"/> -->
       Qiita
+      </a>
+    </div>
+    <div>
+      <a class="text5" href="https://">
       </a>
     </div>
     <div>
@@ -81,6 +86,10 @@ export default class HelloWorld extends Vue {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+a {
+  text-decoration: none;
+  font-size: 70px;
+}
 .header-block {
   display: block;
 }
@@ -103,70 +112,111 @@ export default class HelloWorld extends Vue {
 }
 .cv {
   text-decoration: none;
+  font-size: 15px;
   color:black;
 }
 .text1 {
-  font-size: 70px;
   font-family: arial black;
   color: dodgerblue;
   transition: 1.0s;
-  text-decoration: none;
 }
 .text1:hover {
   color: black;
-  transition: 1.0s;
-  text-decoration: none;
-}
+  transition: 1.0s;}
 .text2 {
-  font-size: 70px;
   color: dodgerblue;
-  text-decoration: none;
 }
 .text2:hover {
   color: black;
   transition: 1.0s;
-  text-decoration: none;
 }
 .text3 {
   font-family: arial black;
-  font-size: 70px;
   color: #00bfff;
-  text-decoration: none;
 }
 .text3:hover {
   color: black;
   transition: 1.0s;
-  text-decoration: none;
 }
 .text4 {
-  font-size: 70px;
   color: #87ceeb;
-  text-decoration: none;
 }
 .text4:hover {
   color: black;
   transition: 1.0s;
-  text-decoration: none;
 }
 .text5 {
   font-family: arial black;
-  font-size: 70px;
   color: #66cdaa;
-  text-decoration: none;
 }
 .text5:hover {
   color: black;
   transition: 1.0s;
-  text-decoration: none;
 }
 .text6 {
-  font-size: 70px;
   color: #00BB00;
-  text-decoration: none;
 }
-.text4:hover {
+.text6:hover {
   color: black;
   transition: 1.0s;
-  text-decoration: none;
+}
+.loader,
+.loader:before,
+.loader:after {
+  background: dodgerblue;
+  -webkit-animation: load1 1s infinite ease-in-out;
+  animation: load1 1s infinite ease-in-out;
+  width: 1em;
+  height: 4em;
+}
+.loader {
+  color: dodgerblue;
+  text-indent: -9999em;
+  margin: 88px auto;
+  position: relative;
+  font-size: 11px;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.08s;
+  animation-delay: -0.08s;
+}
+.loader:before,
+.loader:after {
+  position: absolute;
+  top: 0;
+  content: '';
+}
+.loader:before {
+  left: -1.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 1.5em;
+}
+@-webkit-keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
+}
+@keyframes load1 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 0;
+    height: 4em;
+  }
+  40% {
+    box-shadow: 0 -2em;
+    height: 5em;
+  }
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -12,13 +12,13 @@
         <p>サーバサイド、インフラ、フロント、データ分析の仕事に取り組んできました。</p>
         <p>得意なのはサーバサイドです。</p>
         <p>Go言語でフルスクラッチでサーバサイドの開発をしたことがあります。</p>
-        <p>経験してきた言語は、Ruby、Python、Go、Vue*TypeScript、Node*TypeScriptです。特にGo言語が好きです。</p>
+        <p>経験してきた言語は、Ruby、Python、Go、Vue×TypeScript、Node×TypeScriptです。特にGo言語が好きです。</p>
         <p>またアークテクチャーの設計も好きです。</p>
         <p>技術を通して社会に感動を届けることを意識して日々頑張っています。</p>
       <p></p>
       </div>
       <div>
-        <img src="../../public/yukato.jpg" width="300px" height="300px"/>
+        <img src="../../public/yukato.jpg"/>
       </div>
       <div style="text-align:left">
         <p>I'm a student of Chiba University,mejoring Liberal Art.</p>
@@ -26,7 +26,7 @@
         <p>I worked at serverside,infra and front sometimes data analysis.</p>
         <p>I have strong knowledge about serverside.</p>
         <p>I used to develop api server from scratch with golang.</p>
-        <p>Using Ruby、Python、Go、Vue*TypeScript、Node*TypeScript. My lover is Go.</p>
+        <p>Using Ruby、Python、Go、Vue×TypeScript、Node×TypeScript. My lover is Go.</p>
         <p>I also like design for scalable architecture.(CleanArchitecture,DDD)</p>
         <p>My purpose is causing repercussions with using advance technology.</p>
       </div>
@@ -35,8 +35,21 @@
 </template>
 
 <style scoped>
+@media screen and (max-width:480px){
+  .image {
+    width: 100%;
+    }
+}
 .boxes {
   display: flex;
+}
+p {
+  font-size: 15px;
+  margin: 20px;
+}
+img {
+  width: 500px;
+  height: 500px;
 }
 </style>
 
